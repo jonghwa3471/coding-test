114af2f54a2e6e16b839ca75af4741957954767c
Jonghwa <whdghk3471@gmail.com>
2025-01-14 20:30:50 +0900

    [level 0] Title: 각도기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -0,0 +1,14 @@
+const solution = (angle) => {
+    switch (true) {
+        case 0 < angle && angle < 90:
+            return 1;
+        case angle === 90:
+            return 2;
+        case 90 < angle && angle < 180:
+            return 3;
+        case angle === 180:
+            return 4;
+        default:
+            return "Choose valid angle please :)"
+    }
+}
