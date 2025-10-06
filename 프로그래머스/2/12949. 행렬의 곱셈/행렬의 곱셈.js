c40fa0b2b61129ebbad289d3922ecfea50730767
Jonghwa <whdghk3471@gmail.com>
2025-10-06 16:45:46 +0900

    [level 2] Title: 행렬의 곱셈, Time: 7.59 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -4,17 +4,17 @@ function solution(arr1, arr2) {
     const r2 = arr2.length;
     const c2 = arr2[0].length;
     
-    const result = [];
+    let result = [];
     
     for(let i = 0; i < r1; i++) {
-        result.push(Array(c2).fill(0))
+        result.push(new Array(c2).fill(0));
     }
     
     for(let i = 0; i < r1; i++) {
-        for (let j = 0; j < c2; j++) {
-           for (let k = 0; k < c1; k++) {
-            result[i][j] += arr1[i][k] * arr2[k][j];
-           }
+        for(let j = 0; j < c2; j++) {
+            for(let k = 0; k < c1; k++) {
+                result[i][j] += arr1[i][k] * arr2[k][j];
+            }
         }
     }
     return result;
