03d307ddfb2f3453757c30c891cdd09a57a1d3f4
Jonghwa <whdghk3471@gmail.com>
2025-10-06 18:38:37 +0900

    [level 2] Title: 괄호 회전하기, Time: 12.06 ms, Memory: 37.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -0,0 +1,33 @@
+function solution(s) {
+    let answer = 0;
+    for(let i = 0; i < s.length; i++) {
+        const stack = [];
+        let isCorrect = true;
+        for(let j = 0; j < s.length; j++) {
+            const c = s[(i + j) % s.length];
+            if (c === "[" || c === "{" || c === "(") {
+                stack.push(c);
+            } else {
+                if(stack.length === 0) {
+                    isCorrect = false;
+                    break;
+                } 
+                let top = stack[stack.length - 1];
+                if (c === "]" && top === "[") {
+                    stack.pop();
+                } else if (c === "}" && top === "{") {
+                    stack.pop();
+                } else if (c === ")" && top === "(") {
+                    stack.pop();
+                } else {
+                    isCorrect = false;
+                    break;
+                }
+            }
+        }
+        if (stack.length === 0 && isCorrect) {
+            answer += 1;   
+        }
+    }
+    return answer;
+}
