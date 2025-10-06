3a36b4e4557c2765cb555d29e77235fd3ec2564b
Jonghwa <whdghk3471@gmail.com>
2025-10-06 17:45:30 +0900

    [level 2] Title: 방문 길이, Time: 1.21 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -1,4 +1,4 @@
-function updateLocation(x, y, dir) {
+function updateMove(x, y, dir) {
     switch(dir) {
         case "U":
             return [x, y + 1];
@@ -15,23 +15,20 @@ function validMove(nx, ny) {
     return nx <= 5 && nx >= -5 && ny <= 5 && ny >= -5;
 }
 
-
-
 function solution(dirs) {
     let x = 0;
     let y = 0;
-    
     const visited = new Set();
     
-    for (const dir of dirs) {
-        const [nx, ny] = updateLocation(x, y, dir);
-        if (!validMove(nx, ny)) {
+    for(const dir of dirs) {
+        const [nx, ny] = updateMove(x, y, dir);
+        if(!validMove(nx, ny)) {
             continue;
         }
         visited.add(`${x}${y}${nx}${ny}`);
         visited.add(`${nx}${ny}${x}${y}`);
-
-        [x, y] = [nx, ny];
+        
+        [x, y] = [nx, ny];        
     }
     return visited.size / 2;
 }
