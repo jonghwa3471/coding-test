fb62bab53eaa7a7876ab907ae37777083d0c1b24
Jonghwa <whdghk3471@gmail.com>
2025-10-30 08:37:32 +0900

    [Silver III] Title: N과 M (2), Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -2,15 +2,12 @@ N, M = map(int, input().split())
 
 result = []
 
-def dfs(start: int) -> None:
+def dfs(start):
     if len(result) == M:
         print(*result)
-        return
     for i in range(start, N + 1):
         result.append(i)
         dfs(i + 1)
         result.pop()
-
-dfs(1)
         
-    
+dfs(1)
