c7ffb70f97e3007dc8ba9f88bfc17cb8f0a76e63
Jonghwa <whdghk3471@gmail.com>
2025-10-30 15:20:04 +0900

    [D2] Title: 간단한 369게임, Time: 60 ms, Memory: 51,456 KB -BaekjoonHub

---
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.py
@@ -0,0 +1,16 @@
+T = int(input())
+
+rule = ["3", "6", "9"]
+result = []
+
+for i in range(1, T + 1):
+    arr = list(str(i))
+    store = ""
+    for num in arr:
+        if num in rule:
+            store += "-"
+    if store:
+        result.append(store)
+    else:
+        result.append(str(i))
+print(*result)
