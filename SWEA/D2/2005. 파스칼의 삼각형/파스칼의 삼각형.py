084dedd0d208e568ed42703b78cecf41002f2908
Jonghwa <whdghk3471@gmail.com>
2025-11-03 14:41:18 +0900

    [D2] Title: 파스칼의 삼각형, Time: 62 ms, Memory: 53,760 KB -BaekjoonHub

---
+++ SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
@@ -0,0 +1,13 @@
+T = int(input())
+
+for t in range(T):
+    N = int(input())
+    arr = [[0] * N for _ in range(N)]
+    for i in range(N):
+        arr[i][0] = 1
+        for j in range(1, i + 1):
+            arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j]
+    print(f"#{t + 1}")
+    for row in arr:
+        row = [item for item in row if item != 0]
+        print(*row)
