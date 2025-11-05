e1c156c2f85275204226add3a91edb34efaa1d53
Jonghwa <whdghk3471@gmail.com>
2025-11-05 10:36:38 +0900

    [D2] Title: Base64 Decoder, Time: 59 ms, Memory: 51,328 KB -BaekjoonHub

--- SWEA/D2/1928. Base64 Decoder/Base64 Decoder.py
+++ SWEA/D2/1928. Base64 Decoder/Base64 Decoder.py
@@ -1,13 +1,12 @@
 T = int(input())
 
 for t in range(T):
-    table = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
     S = input()
+    table = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
     binary = ""
     result = ""
     for s in S:
-        index = table.index(s)
-        binary += format(index, "06b")
+        binary += format(table.index(s), "06b")
     for i in range(0, len(binary), 8):
-        result += (chr(int(binary[i : i + 8], 2)))
+        result += chr(int(binary[i : i + 8], 2))
     print(f"#{t + 1} {result}")
