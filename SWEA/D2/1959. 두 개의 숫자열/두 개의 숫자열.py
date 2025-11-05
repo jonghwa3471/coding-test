f6a268dc8fe6eda477716af08cad850a8a65f7a5
Jonghwa <whdghk3471@gmail.com>
2025-11-05 23:05:18 +0900

    [D2] Title: 두 개의 숫자열, Time: 59 ms, Memory: 52,480 KB -BaekjoonHub

---
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.py
@@ -0,0 +1,20 @@
+T = int(input())
+
+for t in range(T):
+    N, M = map(int, input().split())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+    K = 0
+    result = 0
+    if N < M:
+        K = M - N
+    else:
+        K = N - M
+        A, B = B, A
+    for i in range(K + 1):
+        sum_ = 0
+        for j in range(len(A)):
+            sum_ += A[j] *B[i + j]
+        if sum_ >= result:
+            result = sum_
+    print(f"#{t + 1} {result}")
