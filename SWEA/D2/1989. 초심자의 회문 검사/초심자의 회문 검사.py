b8d61134e156d90870efc70064e00133e95f8479
Jonghwa <whdghk3471@gmail.com>
2025-11-07 18:26:57 +0900

    [D2] Title: 초심자의 회문 검사, Time: 65 ms, Memory: 52,480 KB -BaekjoonHub

--- SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.py
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.py
@@ -1,8 +1,8 @@
 T = int(input())
 
 for t in range(T):
-    original = input()
-    if original == original[::-1]:
+    s = input()
+    if s == s[::-1]:
         print(f"#{t + 1} 1")
     else:
         print(f"#{t + 1} 0")
