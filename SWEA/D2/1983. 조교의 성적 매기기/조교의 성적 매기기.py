ff972ea455db7754f3dcfe07ea25df0f5985d81f
Jonghwa <whdghk3471@gmail.com>
2025-11-07 18:19:27 +0900

    [D2] Title: 조교의 성적 매기기, Time: 70 ms, Memory: 52,352 KB -BaekjoonHub

--- SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.py
+++ SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.py
@@ -1,14 +1,14 @@
 T = int(input())
 
 for t in range(T):
-    grades = ["A+", "A0", "A-", "B+", "B0", "B-", "C+", "C0", "C-", "D0"]
     N, K = map(int, input().split())
+    grades = ["A+", "A0", "A-", "B+", "B0", "B-", "C+", "C0", "C-", "D0"]
     scores = []
     for i in range(1, N + 1):
-        mid, fin, hw = map(int, input().split())
-        total = 0.35 * mid + 0.45 * fin + 0.20 * hw
+        mid, fin, hm = map(int, input().split())
+        total = mid * 0.35 + fin * 0.45 + hm * 0.2
         scores.append((i, total))
-    scores.sort(key = lambda item: item[1], reverse = True)
+    scores.sort(lambda x: x[1], reverse = True)
     rank_of = {sid: rank for rank, (sid, _) in enumerate(scores)}
     group = N // 10
     r = rank_of[K]
