fa379b8adf57950a517142ea2698bd0ce01d622e
Jonghwa <whdghk3471@gmail.com>
2025-11-08 14:56:05 +0900

    [D2] Title: 가랏! RC카!, Time: 58 ms, Memory: 52,608 KB -BaekjoonHub

--- SWEA/D2/1940. 가랏！ RC카！/가랏！ RC카！.py
+++ SWEA/D2/1940. 가랏！ RC카！/가랏！ RC카！.py
@@ -9,12 +9,11 @@ for t in range(T):
         c = list(map(int, input().split()))
         command.append(c)
     for item in command:
-        if len(item) == 2:
-            if item[0] == 1:
-                current_speed += item[1]
-            elif item[0] == 2:
-                current_speed -= item[1]
-                if current_speed < 0:
-                    current_speed = 0
+        if item[0] == 1:
+            current_speed += item[1]
+        elif item[0] == 2:
+            current_speed -= item[1]
+            if current_speed < 0:
+                current_speed = 0
         distance += current_speed
     print(f"#{t + 1} {distance}")
