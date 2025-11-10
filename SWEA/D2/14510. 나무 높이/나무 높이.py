0051b8d330c3dfa9ac7047885018a652fd169317
Jonghwa <whdghk3471@gmail.com>
2025-11-10 18:01:46 +0900

    [D2] Title: 나무 높이, Time: 72 ms, Memory: 53,760 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.py
+++ SWEA/D2/14510. 나무 높이/나무 높이.py
@@ -12,10 +12,10 @@ for t in range(T):
         if gap % 2:
             odd += 1
     days = (total_growth // 3) * 2 + (total_growth % 3)
-    one = (days // 2) + (days % 2)
-    two = days // 2
-    if odd <= one:
-        result = days
-    else:
+    one = days // 2 + days % 2
+    result = 0
+    if odd > one:
         result = 2 * odd - 1
+    else:
+        result = days
     print(f"#{t + 1} {result}")
