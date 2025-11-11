343b64695ab0d099cb5b2f5c7c84a9599ddbdd8b
Jonghwa <whdghk3471@gmail.com>
2025-11-11 10:10:49 +0900

    [D3] Title: 24시간, Time: 111 ms, Memory: 59,648 KB -BaekjoonHub

--- SWEA/D3/12368. 24시간/24시간.py
+++ SWEA/D3/12368. 24시간/24시간.py
@@ -2,5 +2,7 @@ T = int(input())
 
 for t in range(T):
     A, B = map(int, input().split())
-    result = (A + B) % 24
-    print(f"#{t + 1} {result}")
+    if A + B >= 24:
+        print(f"#{t + 1} {(A + B) - 24}")
+    else:
+        print(f"#{t + 1} {A + B}")
