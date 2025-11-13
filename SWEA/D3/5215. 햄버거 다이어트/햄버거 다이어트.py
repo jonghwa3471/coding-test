98a8bdcba7b18fc0a6c12565f14443548d12b1d3
Jonghwa <whdghk3471@gmail.com>
2025-11-13 16:21:58 +0900

    [D3] Title: 햄버거 다이어트, Time: 428 ms, Memory: 69,376 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
@@ -3,12 +3,22 @@ T = int(input())
 for t in range(T):
     N, L = map(int, input().split())
     arr = []
-    dp = [0] * (L + 1)
+    result = 0
     for n in range(N):
         T, K = map(int, input().split())
         arr.append((T, K))
-    for taste, cal in arr:
-        for c in range(L, cal - 1, -1):
-            dp[c] = max(dp[c], dp[c - cal] + taste)
-    result = max(dp)
+        
+    def dfs(index, cur_taste, cur_cal):
+        global result
+        if cur_cal > L:
+            return
+        if index == N:
+            if cur_taste > result:
+                result = cur_taste
+            return
+        taste, cal = arr[index]
+        dfs(index + 1, cur_taste + taste, cur_cal + cal)
+        dfs(index + 1, cur_taste, cur_cal)
+        
+    dfs(0, 0, 0)
     print(f"#{t + 1} {result}")
