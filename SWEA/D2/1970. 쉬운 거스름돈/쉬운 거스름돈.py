2f523c1ccbadf2a6d11f29e91f79f3e7439674c1
Jonghwa <whdghk3471@gmail.com>
2025-11-14 20:04:00 +0900

    [D2] Title: 쉬운 거스름돈, Time: 65 ms, Memory: 52,864 KB -BaekjoonHub

--- SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.py
+++ SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.py
@@ -2,10 +2,11 @@ T = int(input())
 
 for t in range(T):
     N = int(input())
-    result = [0] * 8
-    offset = [50000, 10000, 5000, 1000, 500, 100, 50, 10]
-    for i in range(len(offset)):
-        result[i] = N // offset[i]
-        N %= offset[i]
+    arr = [50000, 10000, 5000, 1000, 500, 100, 50, 10]
+    result = [0, 0, 0, 0, 0, 0, 0, 0]
+    
+    for i in range(8):
+        result[i] = N // arr[i]
+        N %= arr[i]
     print(f"#{t + 1}")
     print(*result)
