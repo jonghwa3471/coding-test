1f58a4c1f61b3edeca523e98c1810a1ee26273db
Jonghwa <whdghk3471@gmail.com>
2025-11-14 19:46:53 +0900

    [D2] Title: 조교의 성적 매기기, Time: 75 ms, Memory: 59,264 KB -BaekjoonHub

--- SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.py
+++ SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.py
@@ -1,16 +1,17 @@
 T = int(input())
 
 for t in range(T):
+    grade = ["A+", "A0", "A-", "B+", "B0", "B-", "C+", "C0", "C-", "D0"]
     N, K = map(int, input().split())
-    grades = ["A+", "A0", "A-", "B+", "B0", "B-", "C+", "C0", "C-", "D0"]
-    scores = []
-    for i in range(1, N + 1):
-        mid, fin, hm = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    score = []
+    for i in range(N):
+        mid, fin, hm = arr[i]
         total = mid * 0.35 + fin * 0.45 + hm * 0.2
-        scores.append((i, total))
-    scores.sort(key = lambda x: x[1], reverse = True)
-    rank_of = {sid: rank for rank, (sid, _) in enumerate(scores)}
+        score.append((i + 1, total))
+    score.sort(lambda x: x[1], reverse = True)
+    rank_of = {num: rank for rank, (num, _) in enumerate(score)}
     group = N // 10
     r = rank_of[K]
-    result = grades[r // group]
+    result = grade[r // group]
     print(f"#{t + 1} {result}")
