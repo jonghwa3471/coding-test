6ead54ccba52a269ec12518762c53bb8319161d1
Jonghwa <whdghk3471@gmail.com>
2025-11-14 18:42:11 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 79 ms, Memory: 59,264 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -1,29 +1,30 @@
-T= int(input())
+T = int(input())
 
 for t in range(T):
     N, K = map(int, input().split())
     arr = [list(map(int, input().split())) for _ in range(N)]
-    count = 0
+    result = 0
     for i in range(N):
-        sum_ = 0
+        count = 0
         for j in range(N):
             if arr[i][j] == 1:
-                sum_ += 1
-            else:
-                if sum_ == K:
-                    count += 1
-                sum_ = 0
-        if sum_ == K:
-            count += 1
+                count += 1
+            if arr[i][j] == 0:
+                if count == K:
+                    result +=1
+                count = 0
+        if count == K:
+            result += 1
+            
     for i in range(N):
-        sum_ = 0
+        count = 0
         for j in range(N):
             if arr[j][i] == 1:
-                sum_ += 1
-            else:
-                if sum_ == K:
-                    count += 1
-                sum_ = 0
-        if sum_ == K:
-            count += 1
-    print(f"#{t + 1} {count}")
+                count += 1
+            if arr[j][i] == 0:
+                if count == K:
+                    result += 1
+                count = 0
+        if count == K:
+            result += 1
+    print(f"#{t + 1} {result}")
