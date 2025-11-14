e63e8170fddd8c7969da810d9bf34794dd9e1e3a
Jonghwa <whdghk3471@gmail.com>
2025-11-14 16:27:47 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 80 ms, Memory: 56,192 KB -BaekjoonHub

--- SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.py
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.py
@@ -1,12 +1,13 @@
 T = int(input())
 
-for i in range(T):
+for t in range(T):
     N = int(input())
     arr = list(map(int, input().split()))
-    count  = [0] * (len(arr) + 1)
-    for number in arr:
-        count[number] += 1
-    max_num = max(count)
-    indexs = [idx for idx, value in enumerate(count) if value == max_num]
+    length = len(arr)
+    arr_with_index = [0] * (length + 1)
+    for i in arr:
+        arr_with_index[i] += 1
+    most = max(arr_with_index)
+    indexs = [index for index, value in enumerate(arr_with_index) if value == most]
     result = max(indexs)
-    print(f"#{i + 1} {result}")
+    print(f"#{t + 1} {result}")
