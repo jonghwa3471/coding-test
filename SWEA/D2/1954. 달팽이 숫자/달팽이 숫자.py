ef20c705122da478e331b5259a7d7e7802883d02
Jonghwa <whdghk3471@gmail.com>
2025-11-14 16:19:26 +0900

    [D2] Title: 달팽이 숫자, Time: 62 ms, Memory: 52,224 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -6,11 +6,11 @@ for t in range(T):
     dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
     d = 0
     r, c = 0, 0
-    for i in range(1, (N * N) + 1):
+    for i in range(1, N * N + 1):
         arr[r][c] = i
         nr, nc = r + dirs[d][0], c + dirs[d][1]
         if not (0 <= nr < N and 0 <= nc < N) or arr[nr][nc] != 0:
-            d = (d + 1) % 4
+            d = (d + 1) % len(dirs)
             nr, nc = r + dirs[d][0], c + dirs[d][1]
         r, c = nr, nc
     print(f"#{t + 1}")
