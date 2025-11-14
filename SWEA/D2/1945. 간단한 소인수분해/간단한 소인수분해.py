61736d02ab1583bc51246f5d067ab5192b8aa89e
Jonghwa <whdghk3471@gmail.com>
2025-11-14 17:35:32 +0900

    [D2] Title: 간단한 소인수분해, Time: 62 ms, Memory: 50,944 KB -BaekjoonHub

--- SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
+++ SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
@@ -2,10 +2,10 @@ T = int(input())
 
 for t in range(T):
     N = int(input())
-    arr = [0, 0, 0, 0, 0]
-    division = [2, 3, 5, 7, 11]
-    for i in range(5):
-        while N % division[i] == 0:
-            N //= division[i]
-            arr[i] += 1
-    print(f"#{t + 1}", *arr)
+    arr = [2, 3, 5, 7, 11]
+    result = [0, 0, 0, 0, 0]
+    for i in range(len(arr)):
+        while N % arr[i] == 0:
+            N //= arr[i]
+            result[i] += 1
+    print(f"#{t + 1}", *result)
