4343a46a0421dc777fdcf166509ceabb6121bb33
Jonghwa <whdghk3471@gmail.com>
2025-11-14 18:56:57 +0900

    [D2] Title: 숫자 배열 회전, Time: 65 ms, Memory: 52,352 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
@@ -3,15 +3,18 @@ T = int(input())
 for t in range(T):
     N = int(input())
     arr = [input().split() for _ in range(N)]
+    
     deg90 = [[""] * N for _ in range(N)]
     deg180 = [[""] * N for _ in range(N)]
     deg270 = [[""] * N for _ in range(N)]
+    
     for i in range(N):
         for j in range(N):
             deg90[i][j] = arr[N - 1 - j][i]
             deg180[i][j] = arr[N - 1 - i][N - 1 - j]
             deg270[i][j] = arr[j][N - 1 - i]
-    print(f"#{t + 1}")
+            
+    print(f"#{t + 1}")        
     for i in range(N):
         row90 = "".join(deg90[i])
         row180 = "".join(deg180[i])
