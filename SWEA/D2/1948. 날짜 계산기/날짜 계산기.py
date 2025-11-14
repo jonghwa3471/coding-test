0fb467ff1afc81e2de6eba2275e635896933c6a9
Jonghwa <whdghk3471@gmail.com>
2025-11-14 20:18:10 +0900

    [D2] Title: 날짜 계산기, Time: 59 ms, Memory: 51,712 KB -BaekjoonHub

--- SWEA/D2/1948. 날짜 계산기/날짜 계산기.py
+++ SWEA/D2/1948. 날짜 계산기/날짜 계산기.py
@@ -1,11 +1,12 @@
 T = int(input())
 
 for t in range(T):
-    startMonth, startDay, endMonth, endDay = map(int, input().split())
-    month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    monthTerm = 0
-    dayTerm = endDay - startDay + 1
-    for i in range(startMonth, endMonth):
-        monthTerm += month[i]
-    result = monthTerm + dayTerm
+    arr = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    first_month, first_day, second_month, second_day = map(int, input().split())
+    day = second_day - first_day + 1
+    month = 0
+    for i in range(first_month, second_month):
+        month += arr[i]
+    result = month + day
+    
     print(f"#{t + 1} {result}")
