3e63d3dc5f4e29a48d44a705c2d0ff2cfe793548
Jonghwa <whdghk3471@gmail.com>
2025-11-14 18:06:05 +0900

    [D2] Title: 스도쿠 검증, Time: 68 ms, Memory: 52,608 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -1,30 +1,35 @@
 T = int(input())
 
 for t in range(T):
+    ok = True
     arr = [list(map(int, input().split())) for _ in range(9)]
-    result = 1
     
     for i in range(9):
         if len(set(arr[i])) != 9:
-            result = 0
+            ok = False
             break
-    if result:
+    if ok:
         for i in range(9):
-            cols = []
+            col = set()
             for j in range(9):
-                cols.append(arr[j][i])
-            if len(set(cols)) != 9:
-                result = 0
+                col.add(arr[j][i])
+            if len(col) != 9:
+                ok = False
                 break
-                
-    if result:
+            if not ok:
+                break
+    if ok:
         for i in range(0, 9, 3):
-            for j in range(0, 9, 3):
-                grid = []
+            for j in range(0, 9 , 3):
+                grid = set()
                 for k in range(i, i + 3):
-                    grid.extend(arr[k][j : j + 3])
-                if len(set(grid)) != 9:
-                    result = 0
+                    grid.update(arr[k][j : j + 3])
+                if len(grid) != 9:
+                    ok = False
                     break
-                    
-    print(f"#{t + 1} {result}")
+            if not ok:
+                break
+    if ok:
+        print(f"#{t + 1} 1")
+    else:
+        print(f"#{t + 1} 0")
