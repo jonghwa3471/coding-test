e76a4f1d55e5d02462b022d15ef64f2ec2927b22
Jonghwa <whdghk3471@gmail.com>
2025-11-14 18:18:27 +0900

    [D2] Title: 파스칼의 삼각형, Time: 58 ms, Memory: 51,584 KB -BaekjoonHub

--- SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
+++ SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
@@ -6,8 +6,8 @@ for t in range(T):
     for i in range(N):
         arr[i][0] = 1
         for j in range(1, i + 1):
-            arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j]
+            arr[i][j]  = arr[i - 1][j - 1] + arr[i - 1][j]
     print(f"#{t + 1}")
     for row in arr:
-        result = [item for item in row if item != 0]
-        print(*result)
+        row = [i for i in row if i != 0]
+        print(*row)
