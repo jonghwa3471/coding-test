c8a43402c2bc3daaa0cc89165f91ed4962207c24
Jonghwa <whdghk3471@gmail.com>
2025-11-14 15:33:20 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 743 ms, Memory: 201,004 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
@@ -4,11 +4,10 @@ for t in range(T):
     N = int(input())
     arr = list(map(int, input().split()))
     max_ = 0
-    profit = 0
-    
-    for price in reversed(arr):
-        if max_ <= price:
-            max_ = price
+    result = 0
+    for i in arr[::-1]:
+        if i >= max_:
+            max_ = i
         else:
-            profit += max_ - price
-    print(f"#{t + 1} {profit}")
+            result +=  max_ - i
+    print(f"#{t + 1} {result}")
