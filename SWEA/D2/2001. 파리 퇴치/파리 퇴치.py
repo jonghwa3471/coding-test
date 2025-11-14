42ce5240d1a22fe1aa491e330a62ef1d6f9c6b29
Jonghwa <whdghk3471@gmail.com>
2025-11-14 16:41:40 +0900

    [D2] Title: 파리 퇴치, Time: 77 ms, Memory: 58,368 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -3,12 +3,12 @@ T = int(input())
 for t in range(T):
     N, M = map(int, input().split())
     arr = [list(map(int, input().split())) for _ in range(N)]
-    bugs = 0
+    result = 0
     for i in range(N - M + 1):
-        for j in range(len(arr[0]) - M + 1):
-            sum_ = 0
+        for j in range(N - M + 1):
+            bugs = 0
             for k in range(i, i + M):
-                sum_ += sum(arr[k][j : j + M])
-                if bugs < sum_:
-                	bugs = sum_
-    print(f"#{t + 1} {bugs}")
+                bugs += sum(arr[k][j : j + M])
+            if bugs > result:
+                result = bugs
+    print(f"#{t + 1} {result}")
