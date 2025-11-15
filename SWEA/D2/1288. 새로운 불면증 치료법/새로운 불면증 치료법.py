e57ee7afed8ddb5634147be13a3c82ede2243bd0
Jonghwa <whdghk3471@gmail.com>
2025-11-15 16:12:16 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 79 ms, Memory: 58,624 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
@@ -2,10 +2,10 @@ T = int(input())
 
 for t in range(T):
     N = int(input())
-    arr = set()
-    k = 1
-    while len(arr) < 10:
-        n = N * k
-        arr.update(str(n))
-        k += 1
+    result = 1
+    count = set()
+    while len(count) < 10:
+        n = result * N
+        count.update(list(str(n)))
+        result += 1
     print(f"#{t + 1} {n}")
