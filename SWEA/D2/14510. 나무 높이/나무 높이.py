3808a066191123ca71b3d9b8c7d94fa9d2db6e6e
Jonghwa <whdghk3471@gmail.com>
2025-11-15 16:48:26 +0900

    [D2] Title: 나무 높이, Time: 74 ms, Memory: 59,648 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.py
+++ SWEA/D2/14510. 나무 높이/나무 높이.py
@@ -3,19 +3,17 @@ T = int(input())
 for t in range(T):
     N = int(input())
     trees = list(map(int, input().split()))
-    max_height = max(trees)
+    tallest = max(trees)
     total_growth = 0
     odd = 0
     for tree in trees:
-        gap = max_height - tree
+        gap = tallest - tree
         total_growth += gap
         if gap % 2:
             odd += 1
-    days = (total_growth // 3) * 2 + (total_growth % 3)
+    days = (total_growth // 3) * 2 + total_growth % 3
     one = days // 2 + days % 2
-    result = 0
+    result = days
     if odd > one:
-        result = 2 * odd - 1
-    else:
-        result = days
+        result = odd * 2 - 1
     print(f"#{t + 1} {result}")
