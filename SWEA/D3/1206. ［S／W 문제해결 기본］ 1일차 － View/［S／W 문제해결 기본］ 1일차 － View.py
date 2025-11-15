8647cbbb030ddfc7e4821fcf1f6195fd4ebae6f4
Jonghwa <whdghk3471@gmail.com>
2025-11-15 16:36:16 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 73 ms, Memory: 60,160 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
@@ -1,9 +1,10 @@
 for t in range(10):
     N = int(input())
-    height = list(map(int, input().split()))
-    count = 0
+    heights = list(map(int, input().split()))
+    result = 0
     for i in range(2, N - 2):
-        highest = max(height[i -2], height[i - 1], height[i + 1], height[i + 2])
-        if height[i] > highest:
-            count += height[i] - highest
-    print(f"#{t + 1} {count}")
+        nearby = [heights[i - 2], heights[i - 1], heights[i + 1], heights[i + 2]]
+        tallest = max(nearby)
+        if heights[i] > tallest:
+            result += heights[i] - tallest
+    print(f"#{t + 1} {result}")
