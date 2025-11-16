e706095c7eade8dd5083ff69dc703e08a5f06c7c
Jonghwa <whdghk3471@gmail.com>
2025-11-16 14:02:46 +0900

    [D3] Title: 부분 수열의 합, Time: 78 ms, Memory: 57,984 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
@@ -3,16 +3,10 @@ T = int(input())
 for t in range(T):
     N, K = map(int, input().split())
     A = list(map(int, input().split()))
-    result = 0
-    def dfs(index, cur_sum):
-        global result
-        if cur_sum > K:
-            return
-        if index == N:
-            if cur_sum == K:
-                result += 1
-            return
-        dfs(index + 1, cur_sum)
-        dfs(index + 1, cur_sum + A[index])
-    dfs(0, 0)
+    arr = [0] * (K + 1)
+    arr[0] = 1
+    for val in A:
+        for i in range(K, val - 1, -1):
+            arr[i] += arr[i - val]
+    result = arr[K]
     print(f"#{t + 1} {result}")
